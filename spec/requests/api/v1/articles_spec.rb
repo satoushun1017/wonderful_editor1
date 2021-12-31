ca8bb4ebf00e4eff19b5cd51fc4362517a8dad6c
佐藤駿 <k4a3pgy4e@i.softbank.jp>
2021-12-31 21:57:30 +0900

    let!を使わないものへ修正

--- spec/requests/api/v1/articles_spec.rb
+++ spec/requests/api/v1/articles_spec.rb
@@ -8,17 +8,17 @@ RSpec.describe "Api::V1::Articles", type: :request do
     # let!(:article1) { create(:article, updated_at: 1.days.ago) }
     # let!(:article2) { create(:article, updated_at: 2.days.ago) }
     # let!(:article3) { create(:article) }
-    # before { create(:article, updated_at: 1.days.ago) }
-
-    # before { create(:article, updated_at: 2.days.ago) }
-
-    before { create(:article) }
+    before do
+      create(:article, updated_at: 1.days.ago)
+      create(:article, updated_at: 2.days.ago)
+      create(:article)
+    end
 
     it "記事の一覧が取得できる" do
       subject
       res = JSON.parse(response.body)
       expect(response).to have_http_status(:ok)
-      expect(res.length).to eq 1
+      expect(res.length).to eq 3
       expect(res[0].keys).to eq ["id", "title", "updated_at", "user"]
       expect(res[0]["user"].keys).to eq ["id", "name", "email"]
     end
