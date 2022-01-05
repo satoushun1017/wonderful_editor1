d67bc12b32f840110fd79b7e3cc9e896427b8f84
佐藤駿 <k4a3pgy4e@i.softbank.jp>
2022-01-05 21:09:36 +0900

    Task. 8 フロントエンドとの繋ぎこみ part.1 終了

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,24 @@
+# Rails.application.routes.draw do
+#   namespace :api do
+#     namespace :v1 do
+#       get "articles/index"
+#     end
+#   end
+#   # mount_devise_token_auth_for "User", at: "auth"
+#   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+#   namespace "api" do
+#     namespace "v1" do
+#       mount_devise_token_auth_for "User", at: "auth"
+#       resources :articles
+#     end
+#   end
+# end
+
 Rails.application.routes.draw do
+  root to: "home#index"
+
   namespace :api do
     namespace :v1 do
-      get "articles/index"
-    end
-  end
-  # mount_devise_token_auth_for "User", at: "auth"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  namespace "api" do
-    namespace "v1" do
       mount_devise_token_auth_for "User", at: "auth"
       resources :articles
     end
